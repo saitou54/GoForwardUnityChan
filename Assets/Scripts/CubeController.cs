62a4f6eb0589e9bd2f3c2ad2febc2103205d7af3
RyoSaitou <ryo_saitou@fuji.waseda.jp>
2019-06-19 19:40:18 +0900

    GoForwardUnityChan 初回コミット
    課題の実装まで一通り完了しました。

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+	// 効果音(課題)
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start()
+	{
+		// 効果音を割り当てる(課題)
+		audioSource = gameObject.GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// キューブを移動させる
+		transform.Translate(this.speed, 0, 0);
+
+		// 画面外に出たら破棄する
+		if (transform.position.x < this.deadLine)
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	// キューブの接触時に効果音を再生する(課題)
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "GroundTag" || collision.gameObject.tag == "CubeTag")
+		{
+			audioSource.Play();
+		}
+	}
+}
